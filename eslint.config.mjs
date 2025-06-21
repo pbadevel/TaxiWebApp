d9facc2132353517821097a22821063989faa282
QWERTYZPB <pavelburykin13@gmail.com>
2025-06-21 05:45:41 +0300

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,11 @@ const compat = new FlatCompat({
   rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
+      "@next/next/no-sync-scripts": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@next/next/no-img-element": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off"
     }
 });
 
