640046fb6ae34119154bef906147b86ce7d49cf0
QWERTYZPB <pavelburykin13@gmail.com>
2025-06-21 06:09:02 +0300

    .

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 
 
 const nextConfig: NextConfig = {
-    basePath: '/TalkDrive/',
+    basePath: '/TalkDrive/app',
     assetPrefix: '/TalkDrive/',
     trailingSlash: true,
     
