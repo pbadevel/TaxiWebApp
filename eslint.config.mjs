fba093e387a2e714204423205869560ca6bd9205
QWERTYZPB <pavelburykin13@gmail.com>
2025-06-21 05:48:11 +0300

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,8 +6,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  rules: {
+  baseDirectory: __dirname
+});
+
+const eslintConfig = [
+  ...compat.config(
+    {
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
       "@next/next/no-sync-scripts": "off",
@@ -16,10 +22,7 @@ const compat = new FlatCompat({
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off"
     }
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript")
+    })
 ];
 
 export default eslintConfig;
