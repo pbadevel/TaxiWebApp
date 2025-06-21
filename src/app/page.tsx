580215785811634fc328878ce26694b9cd9e752d
QWERTYZPB <pavelburykin13@gmail.com>
2025-06-21 04:43:40 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,34 @@
 import CustomMapWrapper from '@/components/MapWrapper';
 import styles from '../styles/page.module.css';
+import { useState, useRef, useEffect, useCallback } from 'react';
 
 
 
 // Динамическая загрузка карты
 
 export default function Home() {
+
   
+  useEffect(() => {
+    initializeTelegram();
+  }, []);
+  
+  const initializeTelegram = useCallback(async () => {
+    try {
+      const WebApp = window.Telegram?.WebApp
+
+      if (!WebApp) throw new Error('Telegram WebApp not available');
+      
+      await WebApp.ready();
+      WebApp.expand();
+
+    
+    } catch (error) {
+      console.error('Initialization error:', error);
+      // setLoading(false);
+    }
+    return null;
+  }, []);
 
   
   return (
